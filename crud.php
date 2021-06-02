7e09e53495a7a2d80eb41e58b2d8b29297b5daa7
GUREM-19 <74266183+GUREM-19@users.noreply.github.com>
2021-06-02 13:28:31 -0700

    Add files via upload

--- crud.php
+++ crud.php
@@ -1,15 +1,16 @@
 <?php
-    //CRUD
+    //CRUD with REST
     include 'conexion.php';
 
-    $pdo = new Conexion();
+    $pdo = new Conexion(); //CONEXION A BD
 
+    //REQUESTS
     if($_SERVER['REQUEST_METHOD'] == 'GET'){ //READ
  
-        if(isset($_GET['matricula'])){
-            $sql = $pdo->prepare('SELECT * FROM proyecto.isw WHERE matricula= :matricula' );
+        if(isset($_GET['matricula'])){ //CONSULTAR UN REGISTRO ESPECIFICO DE LA BD 
+            $sql = $pdo->prepare('SELECT * FROM proyecto.isw WHERE matricula= :matricula' ); //QUERY
             $sql->bindValue(':matricula', $_GET['matricula']);
-            $sql-> execute();
+            $sql-> execute(); // EJECUTA LA SENTENCIA PREPARADA
             $sql-> setFetchMode(PDO::FETCH_ASSOC);
 
             header("HTTP/1.1 200 OK");
@@ -17,9 +18,9 @@
             exit;
 
         }
-        else{
-            $sql = $pdo->prepare('SELECT * FROM proyecto.isw' );
-            $sql-> execute();
+        else{ //CONSULTAR TODOS LOS REGISTRO DE LA BD
+            $sql = $pdo->prepare('SELECT * FROM proyecto.isw' );//QUERY
+            $sql-> execute(); // EJECUTA LA SENTENCIA PREPARADA
             $sql-> setFetchMode(PDO::FETCH_ASSOC);
 
             header("HTTP/1.1 200 OK");
@@ -27,14 +28,14 @@
             while ($row = $sql->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)){
                     //ALUMNOS BASE DE DATOS
 			             echo "<tr>";
-                         echo  "<td>$row[0]</td>";
-			             echo  "<td>$row[1]</td>";
-                         echo  "<td>$row[2]</td>";
-                         echo  "<td>$row[3]</td>";
+                         echo  "<td>$row[0]</td>"; //MATRICULA
+			             echo  "<td>$row[1]</td>"; //NOMBRE
+                         echo  "<td>$row[2]</td>"; //APELLIDO
+                         echo  "<td>$row[3]</td>"; //ESTADO
                          echo  "<td><button type='button' name='add_button' class='btn btn-warning btn-xs edit' 
-                         id='$row[0]' data-toggle='modal' data-target='#apicrudModal'>Editar</button></td>";
+                                id='$row[0]' data-toggle='modal' data-target='#apicrudModal'>Editar</button></td>";
                          echo  "<td><button type='button' name='delete' class='btn btn-danger btn-xs delete' 
-                         id='$row[0]'>Eliminar</button></td>";
+                                id='$row[0]'>Eliminar</button></td>";
                          echo  "</tr>";
             }
 
@@ -44,10 +45,11 @@
     }
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){  //CREATE
+        //QUERY
         $sql = "INSERT INTO proyecto.isw (matricula, nombre, apellido, vivo) VALUES ('$_POST[mat]', '$_POST[nombre]', 
         '$_POST[apellido]', '$_POST[estado]')";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
+        $stmt = $pdo->prepare($sql); //PREPARA LA SENTENCIA PARA SU EJECUCION
+        $stmt->execute(); // EJECUTA LA SENTENCIA PREPARADA
         
         header("HTTP/1.1 201 CREATED");
         exit;
@@ -60,12 +62,12 @@
             "status" => 1,
             "answer" => $post_vars
         ));
-
+        //QUERY
         $sql = "UPDATE proyecto.isw SET matricula='$post_vars[matricula]', nombre='$post_vars[nombre]', apellido='$post_vars[apellido]', 
         vivo='$post_vars[estado]' WHERE matricula='$post_vars[mat]'";
         
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
+        $stmt = $pdo->prepare($sql);  //PREPARA LA SENTENCIA PARA SU EJECUCION
+        $stmt->execute(); // EJECUTA LA SENTENCIA PREPARADA
         
         header("HTTP/1.1 204 UPDATED");
         exit;
@@ -73,15 +75,14 @@
     }
     
     if($_SERVER['REQUEST_METHOD']== 'DELETE'){  //DELETE
-        $sql = "DELETE FROM proyecto.isw WHERE matricula=:matricula";
-        $stmt = $pdo->prepare($sql);
+        $sql = "DELETE FROM proyecto.isw WHERE matricula=:matricula"; //QUERY
+        $stmt = $pdo->prepare($sql);  //PREPARA LA SENTENCIA PARA SU EJECUCION
         $stmt->bindValue(':matricula', $_GET['matricula']);
-        $stmt->execute();
+        $stmt->execute(); // EJECUTA LA SENTENCIA PREPARADA
         
         header("HTTP/1.1 200 OK");
         exit;
 
     }
-
 
 ?>
